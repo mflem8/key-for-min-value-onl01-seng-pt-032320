25bce26ce3dad8b57307e52239d7df7ac6ea0917
Matt Fleming <matthewfleming8@gmail.com>
2020-04-02 02:12:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-lowest_key = nil 
-lowest_value = 
+lowest_key = 0 
+lowest_value = nil 
   name_hash.each do |key|
   
       
